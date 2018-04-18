fe4c5506d8db02738592833a017a8cf5c398ebfb
Kathleen E. Giebelhausen <KathleenEGiebelhausen@Kathleens-MacBook-Pro.local>
2018-04-17 18:23:53 -0700

    working with Catherine

--- letter.js
+++ letter.js
@@ -6,9 +6,12 @@
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
 
-var Letter = function(myLetter){
-    this.letter = myLetter,
+
+var Letter = function(letterCharacter){
+    this.letter = letterCharacter,
+    // boolean is false because the player hasn't guess anything yet
     this.isGuessed = false,
+    // this displays either the correct letter or an underscore if the user has not guess this letter yet
     this.display = function(){
         if (this.isGuessed === true){
             return this.letter
@@ -16,6 +19,7 @@ var Letter = function(myLetter){
             return "_"
         }
     },
+    // this checks if the letter was guessed correctly and changes the boolean to true only if it was guessed correctly
     this.checker = function(userGuess){
         if (userGuess === this.letter){
             this.isGuessed = true
@@ -23,10 +27,10 @@ var Letter = function(myLetter){
 
     }
 }
-module.exports = Letter
-var test = new Letter("k");
-// var test2 = new Letter("b");
-console.log(test)
-test.checker("l")
-// console.log(test2)
-console.log(test.display())
+module.exports = Letter;
+// var test = new Letter("k");
+// // var test2 = new Letter("b");
+// console.log(test)
+// test.checker("l")
+// // console.log(test2)
+// console.log(test.display())
--- word.js
+++ word.js
@@ -1,12 +1,60 @@
 var Letter = require("./letter.js")
 
 // Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
+var Word = function(wordPlaceholder){
+    // An array of new Letter objects representing the letters of the underlying word
+    this.lettersInWord = [];
+        
 
+    // looping over the letters in the word to create the array of Letter objects 
+    // this.lettersInWord = [Letter('c'), Letter('a'), Letter('t')]
+    for (var i = 0; i < wordPlaceholder.length; i++) {
+        // this identifies a letter in the word (at a specific index) and stores in the variable called character
+        var character = wordPlaceholder.charAt(i);
+    // console.log(character)
+    // passing in (character) into the new Letter object constructor and asigns that object into the variable of "letter"
+        var letter = new Letter(character);
+        // pushing a new letter object into the lettersInWord array
+        this.lettersInWord.push(letter);
+    }
 
-// An array of new Letter objects representing the letters of the underlying word
-// A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-// A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
+    // console.log(this.lettersInWord);
 
+    // A function that returns a string representing the word.
+    this.displayWord = function(){
+        //This should call the function on each letter object (the first function defined in Letter.js) that 
+        var wordToBeGuessed = "";
+        for (var i = 0; i < this.lettersInWord.length; i++){
+            // displays the character or an underscore and concatenates those together.
+            // accessing the letter at index[i] in the array, and storing it as a variable called letterObject
+            var letterObject = this.lettersInWord[i];
+            // console.log(letterObject.display());
+            wordToBeGuessed = wordToBeGuessed + letterObject.display() + " ";
+        }
 
+        return wordToBeGuessed;
+    };
+   
+    // A function that takes a character as an argument
+    this.letterChecker = function(guessedCharacter){
 
+        for (var i = 0; i < this.lettersInWord.length; i++){
+            var letterObject = this.lettersInWord[i];
+            letterObject.checker(guessedCharacter);
+        }
+    
+    //and calls the guess function on each letter object (the second function defined in Letter.js)
+    }   
+}
 
+module.exports = Word;
+
+var cat = new Word("cat");
+console.log(cat.displayWord());
+cat.letterChecker('a');
+console.log(cat.displayWord());
+// // var test2 = new Word("dog");
+// console.log(test)
+// test.checker("l")
+// // console.log(test2)
+// console.log(test.display())
